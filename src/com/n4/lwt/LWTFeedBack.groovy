71a5e15fe7965567431a37ddc7c4c1d5ee4a5f6d
lekoxnfx <lekoxnfx@qq.com>
2016-01-11 15:29:43 +0800

    添加LWTFeedBack插件（空）

---
+++ src/com/n4/lwt/LWTFeedBack.groovy
@@ -0,0 +1,13 @@
+package com.n4.lwt
+
+import com.navis.argo.business.api.GroovyApi
+import com.navis.services.business.event.GroovyEvent
+
+/**
+ * Created by lekoxnfx on 15/11/11.
+ */
+class LWTFeedBack {
+    public void execute(GroovyEvent inEvent,GroovyApi inApi){
+
+    }
+}
