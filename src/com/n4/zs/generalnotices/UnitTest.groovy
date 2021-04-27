f915d896841887cb46d9dd691d61a1ef9f572c91
leko <lekoxnfx@qq.com>
2021-04-27 19:47:05 +0800

    update ProcessMT0102.groovy

--- src/com/n4/zs/generalnotices/UnitTest.groovy
+++ src/com/n4/zs/generalnotices/UnitTest.groovy
@@ -1,6 +1,7 @@
 package com.n4.zs.generalnotices
 
 import com.navis.argo.business.api.GroovyApi
+import com.navis.inventory.business.moves.WorkInstruction
 import com.navis.services.business.event.GroovyEvent
 
 /**
@@ -15,6 +16,8 @@ class UnitTest {
         com.navis.argo.business.api.ServicesManager sm = (com.navis.argo.business.api.ServicesManager) com.navis.framework.business.Roastery.getBean(com.navis.argo.business.api.ServicesManager.BEAN_ID)
         com.navis.inventory.business.units.Unit u = (com.navis.inventory.business.units.Unit)event.getEntity()
         com.navis.services.business.rules.EventType eventType = com.navis.services.business.rules.EventType.findEventType(postEventName)
+
+        WorkInstruction.createOrUpdateWorkInstructionToPosition()
 
         sm.recordEvent(eventType, note, 1.0, com.navis.argo.business.atoms.ServiceQuantityUnitEnum.UNKNOWN, u)
 
