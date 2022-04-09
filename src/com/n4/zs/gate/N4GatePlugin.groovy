81546d17b53a9ef1e9bcc8eb62ca0af9d6c079a5
lekoxnfx <lekoxnfx@qq.com>
2022-04-09 16:24:04 +0800

    update

--- src/com/n4/zs/gate/N4GatePlugin.groovy
+++ src/com/n4/zs/gate/N4GatePlugin.groovy
@@ -210,14 +210,14 @@ class N4GatePlugin {
         //获取车辆类型成功,开始分类型处理
         if(truckType==null||truckType==""){
             resHint = "获取卡车、卡车公司类型出现异常,请确保录入卡车或卡车公司类型"
-            api.log("行政车辆,直接放行")
+            api.log("获取卡车、卡车公司类型出现异常")
             isAllowed = false
         }
         else if(truckType.equals("行政车辆")||truckType.equals("行政公司")){
             //行政车辆,直接放行
-            api.log("行政车辆,直接放行")
-            isAllowed = true
-            resHint = "行政车辆"
+            api.log("行政车辆,直接禁止")
+            isAllowed = false
+            resHint = "行政车辆,请走行政车道"
 
         }
         else if(truckType.equals("散货车辆")||truckType.equals("仅散货")){
